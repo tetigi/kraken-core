f820966998aa536896588e5a28bd846e98287e22
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-06 23:28:06 +0200

    release 0.2.3

--- src/kraken/api/__init__.py
+++ src/kraken/api/__init__.py
@@ -6,7 +6,7 @@ This file serves as a proxy for Mypy type hints. Importing it in a normal Python
 from kraken.core.build_context import BuildContext
 from kraken.core.project import Project
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 ctx: BuildContext
 project: Project
--- src/kraken/testing/__init__.py
+++ src/kraken/testing/__init__.py
@@ -5,7 +5,7 @@ import pytest
 from kraken.core.build_context import BuildContext
 from kraken.core.project import Project
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 @pytest.fixture

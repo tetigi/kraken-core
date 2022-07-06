0a6ce2a13e2605cf79ee088f7b4c856129e699aa
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-06 15:02:06 +0200

    feature: implement Path value adapters

--- src/kraken/core/property.py
+++ src/kraken/core/property.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import dataclasses
 import warnings
-from typing import Any, ClassVar, Iterable, Mapping, TypeVar
+from pathlib import Path
+from typing import Any, Callable, ClassVar, Iterable, Mapping, TypeVar, cast
 
 import typeapi
 
@@ -46,19 +47,48 @@ class PropertyDescriptor:
     name: str
     default: Any | NotSet
     is_output: bool
+    accepted_types: tuple[type, ...]
 
 
 class Property(Supplier[T]):
     """A property represents an input or output parameter of an :class:`Object`."""
 
-    def __init__(self, owner: Object, name: str) -> None:
+    ValueAdapter = Callable[[Any], Any]
+
+    # This dictionary is a registry for type adapters that are used to ensure that values passed
+    # into a property with :meth:`set()` are of the appropriate type. If a type adapter for a
+    # particular type does not exist, a basic type check is performed. Note that the type adaptation
+    # is not particularly sophisticated at this point and will not apply on items in nested structures.
+    VALUE_ADAPTERS: ClassVar[dict[type, ValueAdapter]] = {}
+
+    def __init__(self, owner: Object, name: str, accepted_types: tuple[type, ...]) -> None:
+
+        # Ensure that we have value adapters for every accepted type.
+        for accepted_type in accepted_types:
+            if accepted_type not in self.VALUE_ADAPTERS:
+                raise ValueError(f"missing value adapter for type {accepted_type!r}")
+        assert len(accepted_types) > 0
+
         self.owner = owner
         self.name = name
+        self.accepted_types = accepted_types
         self._value: Supplier[T] = Supplier.void()
         self._finalized = False
 
     def __repr__(self) -> str:
         return f"Property({self.owner}.{self.name})"
+
+    def _adapt_value(self, value: Any) -> Any:
+        errors = []
+        for accepted_type in self.accepted_types:
+            adapter = self.VALUE_ADAPTERS[accepted_type]
+            try:
+                return adapter(value)
+            except TypeError as exc:
+                errors.append(exc)
+        if len(errors) == 1:
+            raise errors[0]
+        raise TypeError("\n".join(map(str, errors)))
 
     def derived_from(self) -> Iterable[Supplier[Any]]:
         yield self._value
@@ -74,7 +104,7 @@ class Property(Supplier[T]):
         if self._finalized:
             raise RuntimeError(f"{self} is finalized")
         if not isinstance(value, Supplier):
-            value = Supplier.of(value)
+            value = Supplier.of(self._adapt_value(value))
         self._value = value
 
     def setdefault(self, value: T | Supplier[T]) -> T:
@@ -100,6 +130,16 @@ class Property(Supplier[T]):
             #     self._value = Supplier.of(self.get(), derived_from)
             # except Empty as exc:
             #     self._value = Supplier.void(exc, derived_from)
+
+    @staticmethod
+    def value_adapter(type_: type) -> Callable[[ValueAdapter], ValueAdapter]:
+        """Decorator for functions that serve as a value adapter for the given *type_*."""
+
+        def decorator(func: ValueAdapter) -> ValueAdapter:
+            Property.VALUE_ADAPTERS[type_] = func
+            return func
+
+        return decorator
 
 
 class Object:
@@ -139,7 +179,19 @@ class Object:
 
             # Is the hint pointing to a Property type?
             if isinstance(hint, typeapi.Type) and hint.type == Property:
-                schema[key] = PropertyDescriptor(key, NotSet.Value, is_output)
+                assert len(hint.args) == 1, hint
+
+                # Is the inner type a union?
+                item_type = hint.args[0]
+                if isinstance(item_type, typeapi.Union):
+                    # TODO (@NiklasRosenstein): we just expect that any union member be just a type.
+                    accepted_types = [cast(typeapi.Type, t).type for t in item_type.types]
+                elif isinstance(item_type, typeapi.Type):
+                    accepted_types = [item_type.type]
+                else:
+                    raise RuntimeError(f"Property generic parameter must be a type or union, got {item_type}")
+
+                schema[key] = PropertyDescriptor(key, NotSet.Value, is_output, tuple(accepted_types))
 
             # The attribute is annotated as an output but not actually typed as a property?
             elif is_output:
@@ -154,7 +206,7 @@ class Object:
         """Creates :class:`Properties <Property>` for every property defined in the object's schema."""
 
         for key, desc in self.__schema__.items():
-            setattr(self, key, Property(self, key))
+            setattr(self, key, Property(self, key, desc.accepted_types))
 
     def update(self, _raise: bool = False, **property_values: Any) -> None:
         """Assign the properties in *property_values* to the properties of the given :class:`Object`. Raises a
@@ -170,3 +222,34 @@ class Object:
         for key in property_values.keys() - additional_keys:
             prop: Property[Any] = getattr(self, key)
             prop.set(property_values[key])
+
+
+# Register common value adapters
+
+
+def _type_checking_adapter(type_: type) -> Property.ValueAdapter:
+    def func(value: Any) -> Any:
+        if not isinstance(value, type_):
+            raise TypeError(f"expected {type_.__name__}, got {type(value).__name__}")
+        return value
+
+    func.__name__ = f"check_{type_.__name__}"
+    return func
+
+
+Property.value_adapter(str)(_type_checking_adapter(str))
+Property.value_adapter(int)(_type_checking_adapter(int))
+Property.value_adapter(bool)(_type_checking_adapter(bool))
+Property.value_adapter(list)(_type_checking_adapter(list))
+Property.value_adapter(dict)(_type_checking_adapter(dict))
+Property.value_adapter(set)(_type_checking_adapter(set))
+Property.value_adapter(type(None))(_type_checking_adapter(type(None)))
+
+
+@Property.value_adapter(Path)
+def _adapt_path(value: Any) -> Path:
+    if isinstance(value, str):
+        return Path(value)
+    if not isinstance(value, Path):
+        raise TypeError(f"expected Path, got {type(value).__name__}")
+    return value

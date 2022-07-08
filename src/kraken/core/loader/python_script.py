88afeacf9f634d585497ffdd93657bb0ff5eac52
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-08 17:23:55 +0200

    add `inject_kraken_api_module_for_project()` to `kraken.loader.python_script` module which can be used by the `kraken.testing.kraken_project` fixture

--- src/kraken/core/loader/python_script.py
+++ src/kraken/core/loader/python_script.py
@@ -4,10 +4,11 @@ the context of a script executed by this loader and not otherwise. """
 
 from __future__ import annotations
 
+import contextlib
 import sys
 import types
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterator
 
 from kraken.core.project import Project
 
@@ -35,8 +36,6 @@ class PythonScriptProjectLoader(ProjectLoader):
         return file.suffix == ".py"
 
     def load_script(self, file: Path, project: Project) -> None:
-        api_module_name = "kraken.api"
-
         requirements_file = project.directory / REQUIREMENTS_FILE
         if requirements_file.is_file():
             pip_args = [x for x in requirements_file.read_text().splitlines() if not x.startswith("#") and x.strip()]
@@ -46,25 +45,36 @@ class PythonScriptProjectLoader(ProjectLoader):
                 project.context.pyenv.install(pip_args)
                 project.context.pyenv.activate()
 
-        # Create the temporary replacement for the kraken.api module that the script will import from.
-        api_module: Any = types.ModuleType(api_module_name)
-        api_module.ctx = project.context
-        api_module.project = project
-
-        # In order for @dataclass decorators to work in a Python script loaded by this build, it must be
-        # able to look up the module in sys.modules.
-        module = types.ModuleType(f"_kraken__{project.name}_{id(project)}")
-        module.__file__ = str(file)
-
-        old_module = sys.modules.get(api_module_name)
-        try:
-            sys.modules[api_module_name] = api_module
-            sys.modules[module.__name__] = module
+        with inject_kraken_api_module_for_project(project, file) as (api_module, project_module):
             code = compile(file.read_text(), filename=file, mode="exec")
-            exec(code, vars(module))
-        finally:
-            del sys.modules[module.__name__]
-            if old_module is None:
-                sys.modules.pop(api_module_name)
-            else:
-                sys.modules[api_module_name] = old_module
+            exec(code, vars(project_module))
+
+
+@contextlib.contextmanager
+def inject_kraken_api_module_for_project(
+    project: Project,
+    file: Path,
+) -> Iterator[tuple[types.ModuleType, types.ModuleType]]:
+    api_module_name = "kraken.api"
+
+    # Create the temporary replacement for the kraken.api module that the script will import from.
+    api_module: Any = types.ModuleType(api_module_name)
+    api_module.ctx = project.context
+    api_module.project = project
+
+    # In order for @dataclass decorators to work in a Python script loaded by this build, it must be
+    # able to look up the module in sys.modules.
+    project_module = types.ModuleType(f"_kraken__{project.name}_{id(project)}")
+    project_module.__file__ = str(file)
+
+    old_module = sys.modules.get(api_module_name)
+    try:
+        sys.modules[api_module_name] = api_module
+        sys.modules[project_module.__name__] = project_module
+        yield api_module, project_module
+    finally:
+        del sys.modules[project_module.__name__]
+        if old_module is None:
+            sys.modules.pop(api_module_name)
+        else:
+            sys.modules[api_module_name] = old_module

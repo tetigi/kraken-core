81a6cb0fce2371dd3c647b45501380977fbe2dd1
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-09 21:37:02 +0200

    improvement: improve `kraken.testing` module

--- src/kraken/testing/__init__.py
+++ src/kraken/testing/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import contextlib
 import logging
+import sys
 from pathlib import Path
 from typing import Iterator
 
@@ -17,16 +19,28 @@ __version__ = "0.2.8"
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture
 def kraken_ctx() -> BuildContext:
     return BuildContext(Path(".build"))
 
 
-@pytest.fixture
-def kraken_project(kraken_ctx: BuildContext, request: _pytest.fixtures.FixtureRequest) -> Iterator[Project]:
-    kraken_ctx.root_project = Project("test", request.path.parent, None, kraken_ctx)
-    with inject_kraken_api_module_for_project(kraken_ctx.root_project, request.path) as (api_module, project_module):
+@pytest.fixture(name="kraken_ctx")
+def _kraken_ctx_fixture() -> BuildContext:
+    return kraken_ctx()
+
+
+@contextlib.contextmanager
+def kraken_project(kraken_ctx: BuildContext, path: Path | None = None) -> Iterator[Project]:
+    if path is None:
+        path = Path(sys._getframe(1).f_code.co_filename)
+    kraken_ctx.root_project = Project("test", path.parent, None, kraken_ctx)
+    with inject_kraken_api_module_for_project(kraken_ctx.root_project, path) as (_api_module, _project_module):
         yield kraken_ctx.root_project
+
+
+@pytest.fixture(name="kraken_project")
+def _kraken_project_fixture(kraken_ctx: BuildContext, request: _pytest.fixtures.FixtureRequest) -> Iterator[Project]:
+    with kraken_project(kraken_ctx, request.path) as project:
+        yield project
 
 
 def kraken_execute(ctx: BuildContext, targets: list[Task | str] | str) -> None:

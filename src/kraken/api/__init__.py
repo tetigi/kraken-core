5a70d7e0fe4921cb70f2b4f898e659c609d0a77d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-15 23:42:35 +0200

    release 0.2.17

--- src/kraken/api/__init__.py
+++ src/kraken/api/__init__.py
@@ -6,7 +6,7 @@ This file serves as a proxy for Mypy type hints. Importing it in a normal Python
 from kraken.core.build_context import BuildContext
 from kraken.core.project import Project
 
-__version__ = "0.2.16"
+__version__ = "0.2.17"
 
 ctx: BuildContext
 project: Project

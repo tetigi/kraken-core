f796a2817ef3dc480cf76d137682c41da3284d26
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-20 14:16:13 +0200

    fix tests

--- src/tests/test_properties.py
+++ src/tests/test_properties.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Union
 
 from kraken.core.property import Object, Property
 
@@ -8,11 +9,11 @@ from kraken.core.property import Object, Property
 def test__Property_value_adapter_order_is_semantically_revelant() -> None:
     """Tests that a `str | Path` and `Path | str` property behave differently."""
 
-    prop1: Property[str | Path] = Property(Object(), "prop1", (str, Path))
+    prop1: Property[Union[str, Path]] = Property(Object(), "prop1", (str, Path))
     prop1.set("foo/bar")
     assert prop1.get() == "foo/bar"
 
-    prop2: Property[Path | str] = Property(Object(), "prop2", (Path, str))
+    prop2: Property[Union[Path, str]] = Property(Object(), "prop2", (Path, str))
     prop2.set("foo/bar")
     assert prop2.get() == Path("foo/bar")
 

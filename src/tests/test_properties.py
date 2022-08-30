66bd51ca4f0d528ebed8649c1a4a40a89d82fc69
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-08-30 19:17:41 +0200

    tests: add unit test for subclasses of `Object` that do not have annotations, this fails with `typeapi <= 0.2.1` in Python <=3.9. That is why we now require `typeapi ^0.2.2`

--- src/tests/test_properties.py
+++ src/tests/test_properties.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import List, Union
+from typing import Dict, List, Union
 
 from kraken.core.property import Object, Property
 
@@ -36,3 +36,19 @@ def test__Property_default() -> None:
     assert obj.a.get() is not a_value  # Copied
     assert obj.b.get() == 42
     assert obj.c.is_empty()
+
+
+def test__Property_default_factory_with_subclass() -> None:
+    """Tests that property default factory works with a subclass (a known previous semantic failure case)."""
+
+    class MyObj(Object):
+        b: Property[Dict[str, str]] = Property.default_factory(dict)
+
+    class SubObj(MyObj):
+        pass
+
+    obj = MyObj()
+    assert obj.b.get() == {}
+
+    subobj = SubObj()
+    assert subobj.b.get() == {}

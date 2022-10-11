853b16da03fd61ef8c0f54ff337462db1edde722
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-10-11 12:48:27 +0200

    release 0.10.10

--- src/kraken/core/__init__.py
+++ src/kraken/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.10.9"
+__version__ = "0.10.10"
 
 from kraken.core.context import BuildError, Context, ContextEvent
 from kraken.core.executor import Graph, GraphExecutor, GraphExecutorObserver
